a90febba2de0155f905668c9c97fb7e319348994
kamalhersh <kamal.hersh@gmail.com>
2021-09-28 18:48:00 +0000

    splitting_function

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
 import requests
+import string
+import os
+import urllib 
+import urllib.request as r
 
-x= requests.get("https://container0-cxckasqe4a-uc.a.run.app/")
-y = x.content
-z = str(y)
-z = z.splitlines()
-print (z)
+f = open("listing.txt", "w")
+f.write("")
+f.close()
 
+resp= requests.get("https://container0-cxckasqe4a-uc.a.run.app/")
+getcont = resp.content
+strcont = str(getcont)
+splitcont = strcont.split('\\n')
+for wrtcont in splitcont:
+    f = open("listing.txt", "a")
+    f.write(wrtcont + "\n")
+f.close()
+    
